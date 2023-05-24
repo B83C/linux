21c03fb40f418f01bb7793b7aa848ed0ce47147c
Stephan Gerhold <stephan.gerhold@kernkonzept.com>
2023-05-24 09:35:01 +0200

    cpufreq: qcom-nvmem: Enable virtual power domain devices
    The genpd core ignores performance state votes now from devices that are
    runtime suspended. At the moment nothing ever enables the virtual
    devices created for the cpufreq power domain scaling.
    
    Fix this by enabling the devices after attaching them and use
    dev_pm_syscore_device to ensure the power domain stays on during
    suspend. Since it supplies the CPU we can never turn it off from
    Linux. There are other mechanisms to turn it off when needed, usually
    in the RPM firmware or the cpuidle path.
    
    Signed-off-by: Stephan Gerhold <stephan.gerhold@kernkonzept.com>

--- drivers/cpufreq/qcom-cpufreq-nvmem.c
+++ drivers/cpufreq/qcom-cpufreq-nvmem.c
@@ -26,6 +26,7 @@
 #include <linux/platform_device.h>
 #include <linux/pm_domain.h>
 #include <linux/pm_opp.h>
+#include <linux/pm_runtime.h>
 #include <linux/slab.h>
 #include <linux/soc/qcom/smem.h>
 
@@ -281,6 +282,7 @@ static int qcom_cpufreq_probe(struct platform_device *pdev)
 	}
 
 	for_each_possible_cpu(cpu) {
+		struct device **virt_devs = NULL;
 		struct dev_pm_opp_config config = {
 			.supported_hw = NULL,
 		};
@@ -301,7 +303,7 @@ static int qcom_cpufreq_probe(struct platform_device *pdev)
 
 		if (drv->data->genpd_names) {
 			config.genpd_names = drv->data->genpd_names;
-			config.virt_devs = NULL;
+			config.virt_devs = &virt_devs;
 		}
 
 		if (config.supported_hw || config.genpd_names) {
@@ -310,6 +312,23 @@ static int qcom_cpufreq_probe(struct platform_device *pdev)
 				ret = drv->opp_tokens[cpu];
 				dev_err(cpu_dev, "Failed to set OPP config\n");
 				goto free_opp;
+			}
+		}
+
+		if (virt_devs) {
+			const char * const *name = config.genpd_names;
+			int i;
+
+			for (i = 0; *name; i++, name++) {
+				ret = pm_runtime_resume_and_get(virt_devs[i]);
+				if (ret) {
+					dev_err(cpu_dev, "failed to resume %s: %d\n",
+						*name, ret);
+					goto free_opp;
+				}
+
+				/* Keep CPU power domain on during suspend */
+				dev_pm_syscore_device(virt_devs[i], true);
 			}
 		}
 	}
